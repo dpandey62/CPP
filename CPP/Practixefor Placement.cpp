36ac4f68beb7032e3902df513047d4337918bb25
Dev Pandey <139095228+dpandey62@users.noreply.github.com>
2025-06-30 22:48:20 +0530

    Add files via upload

---
+++ CPP/Practixefor Placement.cpp
@@ -0,0 +1,51 @@
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int x= 7;
+// 
+//	cout <<"x="<<x;
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	cout<<"Dev\npandey";
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int a=20;
+//	int b=39;
+//	int sum = a+b;
+//	cout<<"sum = "<<sum ;
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int x=10,y=89,z=23;
+//	cout<< x+y+z;
+//	return 0;
+//}
+
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int x = 10, y = 8;
+//    int sum;
+//    sum = x + y;
+//    int mult;
+//    mult = x * y;
+//    int div;
+//    div = x / y;
+//
+//    cout << "sum = " << sum << endl; // Added endl for new line
+//    cout << "mul = " << mult << endl; // Added endl for new line
+//    cout << "div = " << div << endl; // Added endl for new line
+//
+//    return 0;
+//}
---
+++ CPP/Untitled1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+//using namespace std;
+using std::cout;
+using std::cin;
+using std:: endl;
+int main()
+{
+	cout << "Enter name "<<endl;
+	int n1=0;
+	cin >> n1;
+	cout << "The enter num" << n1;
+	return 0;
+}
